bd3ba899c63ce5bbf547b192516810a77bc57e5f
Jan Kristián Fišera <ikrisfis@icloud.com>
2025-11-09 02:09:53 -0500

    Reimplemented to use proxies instead of hooks

--- src/Extensions/DirectInputExtension.cpp
+++ src/Extensions/DirectInputExtension.cpp
@@ -5,132 +5,62 @@
 #include "MinHook.h"
 #include "Program.h"
 
-HRESULT(__stdcall *oGetDeviceState)(IDirectInputDevice8*		, DWORD  	  , LPVOID		  ) = nullptr;
-HRESULT __stdcall dGetDeviceState  (IDirectInputDevice8* thisPtr, DWORD cbData, LPVOID lpvData)
-{
-	HRESULT result = oGetDeviceState(thisPtr, cbData, lpvData);
-	if (FAILED(result)) return result;
-
-	if (cbData == sizeof(DIMOUSESTATE)) // mouse
-	{
-		
-	}
-	else if (cbData == sizeof(DIMOUSESTATE2)) // mouse
-	{
-		
-	}
-
-	return result;
-}
-
-HRESULT(__stdcall *oGetDeviceData)(IDirectInputDevice8*		   , DWORD			   , LPDIDEVICEOBJECTDATA	   , LPDWORD		 , DWORD		) = nullptr;
-HRESULT __stdcall dGetDeviceData  (IDirectInputDevice8* thisPtr, DWORD cbObjectData, LPDIDEVICEOBJECTDATA rgdod, LPDWORD pdwInOut, DWORD dwFlags)
-{
-	HRESULT result = oGetDeviceData(thisPtr, cbObjectData, rgdod, pdwInOut, dwFlags);
-	if (FAILED(result)) return result;
-
-	*pdwInOut = 0; // TEST: block input
-	return result;
-}
-
-HRESULT(__stdcall *oCreateDevice)(IDirectInput8*		, REFGUID	   , LPDIRECTINPUTDEVICE8A*			, LPUNKNOWN			 ) = nullptr;
-HRESULT __stdcall dCreateDevice	 (IDirectInput8* thisPtr, REFGUID rguid, LPDIRECTINPUTDEVICE8A* ppDevice, LPUNKNOWN pUnkOuter)
-{
-	HRESULT result = oCreateDevice(thisPtr, rguid, ppDevice, pUnkOuter);
-	if (FAILED(result) || !ppDevice || !*ppDevice) return result;
-
-	CDirectInputExtension* ext = NProgram::Extensions::Get<CDirectInputExtension>();
-	if (!CHECK(ext->IsInitialized())) return result;
-
-	TDirectInputObject dIDevice(*ppDevice);
-	if (dIDevice.HookVTableFunction(9, &dGetDeviceState, (LPVOID*)&oGetDeviceState) &&
-		dIDevice.HookVTableFunction(10, &dGetDeviceData, (LPVOID*)&oGetDeviceData))
-	{
-		ext->DInputDevices.Add(Move(dIDevice));
-	}
-	else
-	{
-		std::cout << TEXT("Hooking IDirectInputDevice8::GetDeviceState failed") << std::endl;
-	}
-
-	return result;
-}
-
 HRESULT(WINAPI *oDirectInput8Create)(HINSTANCE		, DWORD			 , REFIID		 , LPVOID*		 , LPUNKNOWN		  ) = nullptr;
 HRESULT WINAPI dDirectInput8Create	(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID* ppvOut, LPUNKNOWN punkOuter)
 {
 	HRESULT result = oDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 	if (FAILED(result) || !ppvOut || !*ppvOut) return result;
 
-	CDirectInputExtension* ext = NProgram::Extensions::Get<CDirectInputExtension>();
-	if (!ext->IsInitialized()) return result;
-	
-	auto& dInput = ext->DInput;
-
-	dInput.Reset((IDirectInput8*)*ppvOut);
-	if (!dInput.HookVTableFunction(3, &dCreateDevice, (LPVOID*)&oCreateDevice))
-	{
-		dInput.Reset();
-		std::cout << TEXT("Hooking IDirectInput8::CreateDevice failed") << std::endl;
-	}
-	
+	*ppvOut = new CInputPatched((IDirectInput8*)*ppvOut);
 	return result;
 }
 
-namespace NDirectInput
+HRESULT CInputPatched::CreateDevice(REFGUID rguid, LPDIRECTINPUTDEVICE8* lplpDirectInputDevice, LPUNKNOWN pUnkOuter)
 {
-	bool Hook(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc)
-	{
-		if (const MH_STATUS status = MH_CreateHook(targetFunc, detourFunc, originalFunc);
-			!CHECK(status == MH_OK))
-		{
-			std::cout << "CreateHook failed. Status: " << MH_StatusToString(status) << std::endl;
-			return false;
-		}
+	HRESULT result = Super::CreateDevice(rguid, lplpDirectInputDevice, pUnkOuter);
+	if (FAILED(result) || !lplpDirectInputDevice || !*lplpDirectInputDevice) return result;
 
-		if (const MH_STATUS status = MH_EnableHook(targetFunc);
-			!CHECK(status == MH_OK))
-		{
-			std::cout << "EnableHook failed. Status: " << MH_StatusToString(status) << std::endl;
-			return false;
-		}
-
-		return true;
-	}
-
-	bool Unhook(const TArray<LPVOID>& hooks)
-	{
-		bool result = true;
-		for (const LPVOID hook : hooks)
-		{
-			result &= MH_RemoveHook(hook) == MH_OK;
-		}
-		return result;
-	}
+	*lplpDirectInputDevice = new CInputDevicePatched(*lplpDirectInputDevice);
+	return result;
 }
 
+HRESULT CInputPatched::EnumDevices(DWORD dwDevType, LPDIENUMDEVICESCALLBACKA lpCallback, LPVOID pvRef, DWORD dwFlags)
+{
+	return Super::EnumDevices(dwDevType, lpCallback, pvRef, dwFlags);
+}
+
+HRESULT CInputPatched::GetDeviceStatus(REFGUID rguidInstance)
+{
+	return Super::GetDeviceStatus(rguidInstance);
+}
+
+HRESULT CInputPatched::FindDevice(REFGUID rguidClass, LPCSTR pszName, LPGUID pguidInstance)
+{
+	return Super::FindDevice(rguidClass, pszName, pguidInstance);
+}
 
 void CDirectInputExtension::Initialize()
 {
 	Super::Initialize();
-	
+
 	DInput8Handle = GetModuleHandle("dinput8.dll");
 	if (!CHECK(DInput8Handle)) return;
 
-	if (LPVOID targetHook = GetProcAddress(DInput8Handle, "DirectInput8Create");
-		NDirectInput::Hook(targetHook, &dDirectInput8Create, (LPVOID*)&oDirectInput8Create))
+	const LPVOID targetHook = GetProcAddress(DInput8Handle, "DirectInput8Create");
+	if (CHECK(MH_CreateHook(targetHook, &dDirectInput8Create, (LPVOID*)&oDirectInput8Create) == MH_OK) &&
+		CHECK(MH_EnableHook(targetHook) == MH_OK))
 	{
-		ModuleHooks.Add(targetHook);
+		Hooks.Add(targetHook);
 	}
-
-	DInputDevices.Reserve(4);
 }
 
 void CDirectInputExtension::Shutdown()
 {
-	DInputDevices.Empty();
-	DInput.Reset();
-	
-	NDirectInput::Unhook(ModuleHooks);
+	for (const LPVOID hook : Hooks)
+	{
+		CHECK(MH_RemoveHook(hook) == MH_OK);
+	}
+
+	Hooks.Empty();
 	Super::Shutdown();
 }
--- src/Extensions/DirectInputExtension.h
+++ src/Extensions/DirectInputExtension.h
@@ -3,48 +3,25 @@
 #pragma once
 
 #include "ProgramExtension.h"
+#include "DirectInput.h"
 
-#define DIRECTINPUT_VERSION 0x0800
-#include <dinput.h>
-
-namespace NDirectInput
+class CInputDevicePatched : public CDirectInputDevice8Proxy
 {
-	bool Hook(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc);
-	bool Unhook(const TArray<LPVOID>& hooks);
-}
+public:
+	typedef CDirectInputDevice8Proxy Super;
+	using Super::Super;
+};
 
-template<typename T>
-struct TDirectInputObject
+class CInputPatched : public CDirectInput8Proxy
 {
-	TDirectInputObject() = default;
-	TDirectInputObject(T* obj) : Obj(obj) {}
-	~TDirectInputObject() { NDirectInput::Unhook(Hooks); }
+public:
+	typedef CDirectInput8Proxy Super;
+	using Super::Super;
 
-	FORCEINLINE bool IsValid() const { return !!Obj; }
-	
-	FORCEINLINE T& Get() const { return *Obj; }
-	FORCEINLINE T* operator->() const { return Obj; }
-	FORCEINLINE T& operator*() const { return *Obj; }
-
-	bool HookVTableFunction(uint8 idx, LPVOID detourFunc, LPVOID* originalFunc)
-	{
-		void** vTable = *(void***)Obj;
-		void* funcAddr = vTable[idx];
-
-		const bool hooked = NDirectInput::Hook(funcAddr, detourFunc, originalFunc);
-		if (hooked) Hooks.Add(funcAddr);
-		return hooked;
-	}
-
-	FORCEINLINE void Reset(T* obj = nullptr)
-	{
-		NDirectInput::Unhook(Hooks);
-		Hooks.Empty();
-		Obj = obj;
-	}
-	
-	T* Obj = nullptr;
-	TArray<LPVOID> Hooks;
+	STDOVERRIDEMETHODIMP CreateDevice(REFGUID rguid, LPDIRECTINPUTDEVICE8* lplpDirectInputDevice, LPUNKNOWN pUnkOuter);
+	STDOVERRIDEMETHODIMP EnumDevices(DWORD dwDevType, LPDIENUMDEVICESCALLBACK lpCallback, LPVOID pvRef, DWORD dwFlags);
+	STDOVERRIDEMETHODIMP GetDeviceStatus(REFGUID rguidInstance);
+	STDOVERRIDEMETHODIMP FindDevice(REFGUID rguidClass, LPCSTR pszName, LPGUID pguidInstance);
 };
 
 class CDirectInputExtension : public IProgramExtension 
@@ -59,10 +36,7 @@ public:
 	// ~END IProgramExtension interface
 
 	HMODULE DInput8Handle = nullptr;
-	TArray<LPVOID> ModuleHooks;
-
-	TDirectInputObject<IDirectInput8> DInput;
-	TArray<TDirectInputObject<IDirectInputDevice8>> DInputDevices;
+	TArray<LPVOID> Hooks;
 };
 
 DECLARE_EXTENSION(CDirectInputExtension)

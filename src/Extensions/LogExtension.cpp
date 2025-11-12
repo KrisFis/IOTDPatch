82b1fb6050a7272250e4285c79b9167a6796ee26
Jan Kristián Fišera <ikrisfis@icloud.com>
2025-11-12 13:16:01 -0500

    Logging to file

---
+++ src/Extensions/LogExtension.cpp
@@ -0,0 +1,74 @@
+﻿// Copyright Alternity Arts. All Rights Reserved.
+
+#include "LogExtension.h"
+
+#include "Program.h"
+
+BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
+{
+	switch (fdwCtrlType)
+	{
+		case CTRL_C_EVENT:
+		case CTRL_BREAK_EVENT:
+		case CTRL_CLOSE_EVENT:
+		case CTRL_LOGOFF_EVENT:
+		case CTRL_SHUTDOWN_EVENT:
+			NProgram::RequestShutdown(TEXT("CtrlHandler"));
+			return TRUE;
+		default:
+			return FALSE;
+	}
+}
+
+void CLogExtension::Initialize()
+{
+	Super::Initialize();
+
+#if BUILD_DEBUG
+	if (AllocConsole() && freopen_s(&_conOut, "CONOUT$", "w", stdout) == 0)
+	{
+		//std::ios::sync_with_stdio(true);
+		_conBuf = std::cout.rdbuf();
+
+		SetConsoleCtrlHandler(CtrlHandler, TRUE);
+	}
+#endif
+
+	_logFile = std::ofstream(LOG_FILENAME, std::ios::out | std::ios::trunc);
+
+	_dbuf = StreamDualBuf(_conBuf, _logFile.rdbuf());
+	_oldBuf = std::cout.rdbuf(&_dbuf);
+}
+
+void CLogExtension::Shutdown()
+{
+	if (_oldBuf)
+	{
+		std::cout.rdbuf(_oldBuf);
+		_oldBuf = nullptr;
+	}
+
+	if (_logFile.is_open())
+	{
+		_logFile.flush();
+		_logFile.close();
+	}
+
+#if BUILD_DEBUG
+	if (_conOut)
+	{
+		fclose(_conOut);
+		_conOut = nullptr;
+	}
+
+	if (_conBuf)
+	{
+		SetConsoleCtrlHandler(CtrlHandler, FALSE);
+		FreeConsole();
+
+		_conBuf = nullptr;
+	}
+#endif
+
+	Super::Shutdown();
+}
---
+++ src/Extensions/LogExtension.h
@@ -0,0 +1,59 @@
+﻿// Copyright Alternity Arts. All Rights Reserved.
+
+#pragma once
+
+#include "ProgramExtension.h"
+
+#include <fstream>
+
+class CLogExtension : public IProgramExtension
+{
+public:
+	static constexpr const tchar* LOG_FILENAME = TEXT("IOTDPatched.log"); 
+
+	typedef IProgramExtension Super;
+
+	// ~BEGIN IProgramExtension interface
+	virtual void Initialize() override;
+	virtual void Tick(double deltaTime) override {}
+	virtual void Shutdown() override;
+	// ~END IProgramExtension interface
+
+private:
+	class StreamDualBuf : public std::streambuf
+	{
+	public:
+		StreamDualBuf() = default;
+		StreamDualBuf(std::streambuf* a, std::streambuf* b) : sb1(a), sb2(b) {}
+
+	protected:
+		int overflow(int c) override {
+			if (c == EOF) return 0;
+			if (sb1) sb1->sputc(c);
+			if (sb2) sb2->sputc(c);
+			return c;
+		}
+
+		int sync() override {
+			int r1 = sb1 ? sb1->pubsync() : 0;
+			int r2 = sb2 ? sb2->pubsync() : 0;
+			return (r1 == 0 && r2 == 0) ? 0 : -1;
+		}
+
+	private:
+		std::streambuf* sb1 = nullptr;
+		std::streambuf* sb2 = nullptr;
+	};
+
+#if BUILD_DEBUG
+	std::streambuf* _conBuf = nullptr;
+	FILE* _conOut = nullptr;
+#endif
+
+	StreamDualBuf _dbuf;
+	std::streambuf* _oldBuf = nullptr;
+
+	std::ofstream _logFile;
+};
+
+DECLARE_EXTENSION(CLogExtension)
